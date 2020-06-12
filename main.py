135998f36a404c1c568d831badb7e36498fbe224
Kiran Sanganee <kirans@kirans-mbp.home>
2020-06-12 15:15:35 +0100

    complete

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import lxml
 from lxml import html
 
 
-
 def job():
-    url = 'http://traintimes.org.uk/Meldreth/Cambridge/0800/today'
+    url = "http://traintimes.org.uk/Meldreth/Cambridge/0800/today"
 
     r = requests.get(url)
     tree = html.fromstring(r.content)
+
     traintime = tree.xpath('//*[@id="result0"]/strong/text()')
     possibledelayed = tree.xpath('//*[@id="result0"]/a[1]/text()')
 
@@ -17,17 +17,19 @@ def job():
     arrivaltime = times[2]
 
     if possibledelayed[0] == 'iCal':
-        message = "The " + arrivaltime + " is not delayed and will arrive at " + arrivaltime
+        message = "The " + departuretime + \
+            " is not delayed and will arrive at " + arrivaltime
     else:
-        message = "The " + arrivaltime + " is " + possibledelayed[0]
+        message = "The " + departuretime + " is " + possibledelayed[0]
 
     print(message)
 
-    # from pushover import init, Client   
-    # token = 'aqea1ttcxmcty1pkmgpviwbah54tv8'
-    # key = 'unvkmu5bskjfyko57qwg2e7txq87vy'
-    # client = Client(key, api_token=token)
-    # client.send_message(message)
+    from pushover import init, Client
+    token = 'aqea1ttcxmcty1pkmgpviwbah54tv8'
+    key = 'unvkmu5bskjfyko57qwg2e7txq87vy'
+    client = Client(key, api_token=token)
+    client.send_message(message)
+
 
 job()
 
